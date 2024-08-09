4110d46dd3e2acbd8e82f360bc9991171348a08f
mingu-beep <bramurve@gmail.com>
2024-08-09 13:27:28 +0900

    view init
    기본적인 view 설계
    - 추가 예정

---
+++ src/main/java/min/project/howeather/HoweatherApplication.java
@@ -0,0 +1,13 @@
+package min.project.howeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoweatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/min/project/howeather/HoweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package min.project.howeather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoweatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
